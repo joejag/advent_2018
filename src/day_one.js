c74dd35e2e3988411f0d6b475bd551e1814ddb0b
Joe Wright <joe@joejag.com>
2018-12-06 23:31:50 +0000

    Day one part two - ugly

--- src/day_one.js
+++ src/day_one.js
@@ -3,3 +3,24 @@ import _ from 'lodash'
 const stringToNumbers = (number) => number.split(' ').map(Number)
 
 export const addItUp = (mathBits) => _.sum(stringToNumbers(mathBits))
+
+export const findTheFreq = (mathBits) => {
+  var parts = stringToNumbers(mathBits)
+  const seenNumbers = []
+  var currentNumber = parts.shift()
+
+  while (parts.length !== 0) {
+    const nextNumber = parts.shift()
+    currentNumber = currentNumber + nextNumber
+    if (seenNumbers.includes(currentNumber)) {
+      return currentNumber
+    }
+    seenNumbers.push(currentNumber)
+
+    if (parts.length === 0) {
+      parts = stringToNumbers(mathBits)
+    }
+  }
+
+  return currentNumber
+}
--- src/day_one.test.js
+++ src/day_one.test.js
@@ -1,7 +1,10 @@
-import { addItUp } from './day_one'
+import { addItUp, findTheFreq } from './day_one'
 
-describe('adding things', () => {
-  test('+1, +1, -2 results in 0', () => expect(addItUp('+1 +1 -2')).toBe(0))
-  test('+1, +1, +1 results in  3', () => expect(addItUp('+1 +1 +1')).toBe(3))
-  test('-1, -2, -3 results in -6', () => expect(addItUp('-1 -2 -3')).toBe(-6))
-})
+test('+1, +1, -2 results in 0', () => expect(addItUp('+1 +1 -2')).toBe(0))
+test('+1, +1, +1 results in  3', () => expect(addItUp('+1 +1 +1')).toBe(3))
+test('-1, -2, -3 results in -6', () => expect(addItUp('-1 -2 -3')).toBe(-6))
+
+test('+1, -1 first reaches 0 twice.', () => expect(findTheFreq('+1 -1')).toBe(0))
+test('+3, +3, +4, -2, -4 first reaches 10 twice.', () => expect(findTheFreq('+3 +3 +4 -2 -4')).toBe(10))
+test('-6, +3, +8, +5, -6 first reaches 5 twice.', () => expect(findTheFreq('-6 +3 +8 +5 -6')).toBe(5))
+test('+7, +7, -2, -7, -4 first reaches 14 twice.', () => expect(findTheFreq('+7 +7 -2 -7 -4')).toBe(14))
