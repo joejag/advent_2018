9cb87198506eb69ba4d9c47a13ff91b5c73adc1e
Joe Wright <joe@joejag.com>
2018-12-09 21:39:49 +0000

    Day five part two - good, but slow

--- src/day_05.js
+++ src/day_05.js
@@ -1,14 +1,15 @@
 import _ from 'lodash'
 import fs from 'fs'
 
+const POLYMER_TO_REMOVE = ['Aa', 'aA', 'Bb', 'bB', 'Cc', 'cC', 'Dd', 'dD', 'Ee', 'eE', 'Ff', 'fF', 'Gg', 'gG', 'Hh', 'hH', 'Ii', 'iI', 'Jj', 'jJ', 'Kk', 'kK', 'Ll', 'lL', 'Mm', 'mM', 'Nn', 'nN', 'Oo', 'oO', 'Pp', 'pP', 'Qq', 'qQ', 'Rr', 'rR', 'Ss', 'sS', 'Tt', 'tT', 'Uu', 'uU', 'Vv', 'vV', 'Ww', 'wW', 'Xx', 'xX', 'Yy', 'yY', 'Zz', 'zZ']
+
 export const alchemicalReduction = (input) => {
   let result = input
 
-  const thingsToRemove = ['Aa', 'aA', 'Bb', 'bB', 'Cc', 'cC', 'Dd', 'dD', 'Ee', 'eE', 'Ff', 'fF', 'Gg', 'gG', 'Hh', 'hH', 'Ii', 'iI', 'Jj', 'jJ', 'Kk', 'kK', 'Ll', 'lL', 'Mm', 'mM', 'Nn', 'nN', 'Oo', 'oO', 'Pp', 'pP', 'Qq', 'qQ', 'Rr', 'rR', 'Ss', 'sS', 'Tt', 'tT', 'Uu', 'uU', 'Vv', 'vV', 'Ww', 'wW', 'Xx', 'xX', 'Yy', 'yY', 'Zz', 'zZ']
-  const thingsRegEx = new RegExp(thingsToRemove.join('|'))
+  const thingsRegEx = new RegExp(POLYMER_TO_REMOVE.join('|'))
 
   while (result.match(thingsRegEx)) {
-    _.forEach(thingsToRemove, (s) => {
+    _.forEach(POLYMER_TO_REMOVE, (s) => {
       result = result.replace(s, '')
     })
   }
@@ -16,8 +17,23 @@ export const alchemicalReduction = (input) => {
   return result
 }
 
+export const improvePolymer = (input) => {
+  const possibles = _.map(POLYMER_TO_REMOVE, (s) => {
+    let result = input
+    result = result.replace(new RegExp(s[0], 'g'), '')
+    result = result.replace(new RegExp(s[1], 'g'), '')
+    return {
+      letter: s[0],
+      length: alchemicalReduction(result).length
+    }
+  })
+
+  return _.minBy(possibles, 'length')
+}
+
 export default () => {
   const input = fs.readFileSync('./src/day_05.txt').toString()
 
   console.log('5.1', alchemicalReduction(input).length)
+  console.log('5.2', improvePolymer(input))
 }
--- src/day_05.test.js
+++ src/day_05.test.js
@@ -1,7 +1,14 @@
-import { alchemicalReduction } from './day_05'
+import { alchemicalReduction, improvePolymer } from './day_05'
 
 it('aA dies', () => { expect(alchemicalReduction('aA')).toEqual('') })
 it('collides', () => { expect(alchemicalReduction('abBA')).toEqual('') })
 it('does nothing 1', () => { expect(alchemicalReduction('abAB')).toEqual('abAB') })
 it('does nothing 2', () => { expect(alchemicalReduction('aabAAB')).toEqual('aabAAB') })
 it('works bigger', () => { expect(alchemicalReduction('dabAcCaCBAcCcaDA')).toEqual('dabCBAcaDA') })
+
+it('find faulty polymer', () => {
+  expect(improvePolymer('dabAcCaCBAcCcaDA')).toEqual({
+    letter: 'C',
+    length: 4
+  })
+})
